d288f8b9d3a743c0fbcb62d4261afa99621e5d41
Joseph Gilley <j@banditml.com>
2021-12-17 17:17:07 -0800

    ci: add git release plugin

--- release.config.js
+++ release.config.js
@@ -7,4 +7,11 @@ module.exports = {
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true },
   ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
 }
